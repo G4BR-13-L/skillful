8794e3d98bb543f52f33d1c6dfb153cb334be7d7
G4BR-13-L <gabriel.victorc13@gmail.com>
2020-08-16 14:00:13 -0300

    Adicionados os primeiros quotes; Fonte nova para o conteudo motivacional

--- js/motivacionalQuote.js
+++ js/motivacionalQuote.js
@@ -1,22 +1,24 @@
 console.log('Motivacional quote INITIALIZING...');
 const quotes = [
 	{id:1, quoteWords:'May the force be with you', author:'Wise Jedi'},
-	{id:2, quoteWords:'frase2', author:'bruce lee'},
-	{id:3, quoteWords:'frase 3', author:'yoda'},
-	{id:4, quoteWords:'frase 4', author:'shifu'},
-	{id:5, quoteWords:'frase 5', author:'oogoway'},
-	{id:6, quoteWords:'frase 6', author:'Obi Wan'},
+	{id:2, quoteWords:'Be water my friend', author:'Bruce Lee'},
+	{id:3, quoteWords:'Express yourself', author:'Bruce Lee'},
+	{id:4, quoteWords:'A única coisa que cabe a  nós decidir, é o que fazer com o tempo que nos é dado ', author:'Gandalf'},
+	{id:5, quoteWords:'Yesterday you said tomorrow', author:'---'},
+	{id:6, quoteWords:'Make yourself hard to kill', author:'---'},
+	{id:5, quoteWords:'There is no secret ingredient', author:'Kung Fu Panda'},
+	{id:5, quoteWords:`<p>Eles não entendem, não vão descobrir</p> <p>Dominado pela má vontade</p> <p>Tira esse cara de perto de mim</p> <p>Meu compadre, o que tu faz não presta à ninguém</p> <p>Ninguém vai consumir</p> <p>Não se queixe da falta do peixe</p> <p>Não deixe o mar te engolir</p>`, author:'Charlie Brown Jr'}
 ];
 function motivate(){
 	console.log('Motive me!');
 	var quoteList = document.querySelector('ul.ul-quote-list');
 	var newQuote = document.createElement('li');
 	newQuote.setAttribute("class", "motivacional-quotes-list-item");
-	var idQuote = Number.parseInt(Math.random()* 6);
+	var idQuote = Number.parseInt(Math.random()* 8);
 	var objectQuote = quotes[idQuote];
 
-	newQuote.innerHTML =`<p class="quote"> ${objectQuote.quoteWords}</p>
-							<span class="author">${objectQuote.author}</span>`;
+	newQuote.innerHTML =`<div class="quote"> ${objectQuote.quoteWords}</div>
+							<div class="author">- ${objectQuote.author} -</div>`;
 	quoteList.appendChild(newQuote);
 
 }
