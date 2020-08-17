e9107e593915986748b7a1d77d7fced0cff6cbd3
G4BR-13-L <gabriel.victorc13@gmail.com>
2020-08-16 23:55:50 -0300

    Grandes Mudandças: 1.grandes alterações na interface; 2.Adição de um menu animado;3.Novos arquivos de css;4. Icones, fonte de icones; Grandes modificações no elementos e tags html. Bugs: abas não alteram a cor da fonte, icone do botão remover está em binario; Da proxima vez limpar, organizar e comentar o código

--- js/navigation.js
+++ js/navigation.js
@@ -2,37 +2,17 @@ console.log('initializating navigation script...');
 var tabOne = document.querySelector('span.tab-1');
 var tabTwo = document.querySelector('span.tab-2');
 var tabThree = document.querySelector('span.tab-3');
-var negativeBorderOne = document.querySelector('span.negative-radius-1');
-var negativeBorderTwo = document.querySelector('span.negative-radius-2');
-var negativeBorderThree = document.querySelector('span.negative-radius-3');
-var negativeBorderFour = document.querySelector('span.negative-radius-4');
-var whiteGroundOne = document.querySelector('div.white-ground-1');
-var whiteGroundTwo = document.querySelector('div.white-ground-2');
-var whiteGroundThree = document.querySelector('div.white-ground-3');
-var whiteGroundFour = document.querySelector('div.white-ground-4');
 
 var nativefierScriptGenerator = document.querySelector('div.content');
-negativeBorderOne.style.background = '#CCC';
-negativeBorderTwo.style.background = '#CCC';
-tabOne.style.background = '#CCC';
+tabOne.style = 'background-color: blue; color: #209f84';
 
 
 
 function tab1(){
 	
-	tabOne.style.background = '#CCC';
-	tabTwo.style.background = '#FFF';
-	tabThree.style.background = '#FFF';
-	negativeBorderOne.style.background = '#CCC';
-	negativeBorderTwo.style.background = '#CCC; ';
-	negativeBorderThree.style.background = '#FFF';
-	negativeBorderFour.style.background = '#FFF';
-
-	whiteGroundOne.style = 'border-radius: 0 0 10px 0;';
-	whiteGroundTwo.style = 'border-radius: 0 10px 0 0;';
-	whiteGroundThree.style = 'border-radius: 0 0 0 0;';
-	whiteGroundFour.style = 'border-radius: 0 0 0 0;';
-
+	tabOne.style ='background: #fff; color: #209f84';
+	tabTwo.style = 'background: #209f84; color: #fff';
+	tabThree.style = 'background: #209f84; color: #fff';
 	nativefierScriptGenerator.innerHTML = `
 				<div id="center">				
 					<h1>Username</h1>
@@ -45,36 +25,27 @@ function tab1(){
 					<label for="iconBeforeBase64" id="iconBeforeBase64-label">Browse...</label>
 					<input type="file" id="iconBeforeBase64" />
 					<div id="buttons">
-						<button onclick="clearList();" >Clear</button>
-						<button onclick="generate()">Generate</button>
+						<button onclick="clearList();" ><i class="icofont-mop"></i> Clear</button>
+						<button onclick="generate()"><i class="icofont-automation"></i> Generate</button>
 					</div>
 				</div>`;
 	console.log('Nativefier script generator selected');
 }
 function tab2(){
 	
-	tabOne.style.background = '#FFF';
-	tabTwo.style.background = '#CCC';
-	tabThree.style.background = '#FFF';
+	tabOne.style = 'background: #209f84; color: #fff';
+	tabTwo.style ='background: #fff; color: #209f84';
+	tabThree.style = 'background: #209f84; color: #fff';
 
-	negativeBorderOne.style.background = '#FFF';
-	negativeBorderTwo.style.background = '#CCC';
-	negativeBorderThree.style.background = '#CCC';
-	negativeBorderFour.style.background = '#FFF';
-
-	whiteGroundOne.style = 'border-radius: 0 0 0 0;';
-	whiteGroundTwo.style = 'border-radius: 0 0 10px 0;';
-	whiteGroundThree.style = 'border-radius: 0 10px 0 0;';
-	whiteGroundFour.style = 'border-radius: 0 0 0 0;';
-
+	
 	nativefierScriptGenerator.innerHTML = `<div id="second-page">
 						
 						<h1>Application Name</h1>
 						<input class="data-field" type="text" id="hated-application-name">
 						
 						<div id="buttons">
-							<button onclick="clearList();" >Clear</button>
-							<button onclick="generateRmScript()">Generate rm Script</button>
+							<button onclick="clearList();" > <i class="icofont-mop"></i>Clear</button>
+							<button onclick="generateRmScript()"><i class="icofont-automation"></i> Generate rm Script</button>
 						</div>
 					</div>`
 	console.log('Tab 2 selected');
@@ -82,22 +53,33 @@ function tab2(){
 
 function tab3(){
 	
-	tabOne.style.background = '#FFF';
-	tabTwo.style.background = '#FFF';
-	tabThree.style.background = '#CCC';
-
-	negativeBorderOne.style.background = '#FFF';
-	negativeBorderTwo.style.background = '#FFF';
-	negativeBorderThree.style.background = '#CCC';
-	negativeBorderFour.style.background = '#CCC';
-
-	whiteGroundOne.style = 'border-radius: 0 0 0 0;';
-	whiteGroundTwo.style = 'border-radius: 0 0 0 0;';
-	whiteGroundThree.style= 'border-radius: 0 0 10px 0;';
-	whiteGroundFour.style = 'border-radius: 0 10px 0 0;';
+	tabOne.style = 'background: #209f84; color: #fff';
+	tabTwo.style = 'background: #209f84; color: #fff';
+	tabThree.style ='background: #fff; color: #209f84';
 
 	nativefierScriptGenerator.innerHTML = `<h1>Terceira Pagina</h1>`
 	console.log('Tab 3 selected');
+}
+var menuState = 2;
+
+function openMenu(){
+	var roundedPage = document.querySelector('div#script-list');
+	var resumerMotivacional = document.querySelector('div.resumer-motivacional')
+
+	if(menuState%2 === 0){
+		roundedPage.style = 'width:50%;transform:translateX(20%)';
+		resumerMotivacional.style = 'width:25%;';
+		menuState++;
+		console.log(menuState);
+	}else{
+		roundedPage.style = 'width:70%;transform:translateX(5%)';
+		resumerMotivacional.style = 'width:18%;';
+		menuState++;
+		console.log(menuState);
+	}
+
+
+	
 }
 console.log('Navigation script INITIALIZED');
 
